6b13111912b47a03b45ff3861b6c2d7bc2bac492
Joshua Jenkins <jenkia18@mail.lcc.edu>
2017-01-26 19:57:47 -0600

    Drivetrain Stuff

--- src/org/usfirst/frc/team6078/robot/drivetrain/Drivetrain.java
+++ src/org/usfirst/frc/team6078/robot/drivetrain/Drivetrain.java
@@ -1,5 +1,14 @@
 package org.usfirst.frc.team6078.robot.drivetrain;
 
-public class Drivetrain {
+import org.usfirst.frc.team6078.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.RobotDrive;
+
+public class Drivetrain {
+	
+	public static class  DrivetrainV1 {
+	
+		public static RobotDrive drive = new RobotDrive(RobotMap.frontLeftMotor, RobotMap.frontRightMotor, RobotMap.backLeftMotor, RobotMap.backRightMotor);
+	
+	}
 }
