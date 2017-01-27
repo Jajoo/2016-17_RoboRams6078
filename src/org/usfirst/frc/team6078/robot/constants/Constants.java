9697bdb5fda950c1272d9cfa61a944a569cd7e4b
Joshua Jenkins <jenkia18@mail.lcc.edu>
2017-01-26 19:10:28 -0600

    First Commit

---
+++ src/org/usfirst/frc/team6078/robot/constants/Constants.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team6078.robot.constants;
+
+public class Constants {
+	
+	//Drivetrain motor ports on rio 
+	public static double frontLeftMotorPort = 1;
+	public static double frontRightMotorPort = 2;
+	public static double backLeftMotorPort = 3; 
+	public static double backRightMotorPort = 4;
+
+	//Ball Intake
+	public static int ballIntakePort = 5;
+	
+	//Joysticks
+	public static double xboxPort = 0;
+	public static double flightStick = 1;
+}
---
+++ src/org/usfirst/frc/team6078/robot/drivetrain/Drivetrain.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team6078.robot.drivetrain;
+
+public class Drivetrain {
+
+}
