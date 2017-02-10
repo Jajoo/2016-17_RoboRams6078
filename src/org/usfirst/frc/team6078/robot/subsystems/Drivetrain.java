c4b9c56050793a4a63af89881df6a586fc8a6146
Joshua Jenkins <jenkia18@mail.lcc.edu>
2017-02-10 12:20:51 -0600

    Added Turning to alternateDrive, this function is quickly getting out of hand

--- src/org/usfirst/frc/team6078/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team6078/robot/subsystems/Drivetrain.java
@@ -13,12 +13,42 @@ public class Drivetrain {
 	}
 	
 	public static void alernateDrive(int cet) {
-			
+			// -----> Don't set cet greater than 1 or less than -1 <--------
+		
+			//If all you want to do is move forwards or backwards
+			if (cet <= 1) {
 			RobotMap.frontLeftMotor.set(cet);
 			RobotMap.frontRightMotor.set(cet);
 			
 			RobotMap.backLeftMotor.set(cet);
 			RobotMap.backRightMotor.set(cet);
+			}
+			
+			/**
+			 * So like the usual exampleMotor.set() function works on a -1 - 1 scale, this
+			 * turning stuff works on a 2 - 4 scale (it subtracts the number by 3 so it works 
+			 * with .set()). For example, if you plug in 4, the left motors will go forward
+			 * at full speed, and the right motors backward at full speed, turning the robot
+			 * left. If you plug in 2, the right motors will go forward at full speed, the left 
+			 * motors backward at full speed, turning the robot right. 
+			 */
+			else if (cet > 1 && cet <= 4) {
+				
+				int cetLeft;
+				int cetRight;
+				
+				cetLeft = cet - 3;
+				cetRight = -cetLeft;
+				
+				//Turns the left motors one direction
+				RobotMap.frontLeftMotor.set(cetLeft);
+				RobotMap.backLeftMotor.set(cetLeft);
+				
+				//Turns the right motors another direction
+				RobotMap.frontRightMotor.set(cetRight);
+				RobotMap.backRightMotor.set(cetRight);
+				
+			}
 			
 	}
 }
